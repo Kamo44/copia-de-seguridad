7b2085b743aac990efd3be31874c40707e3bd382
Kamo44 <131415914+Kamo44@users.noreply.github.com>
2023-06-05 20:54:50 -0500

    Add files via upload

---
+++ controllers/index.controller.js
@@ -0,0 +1,22 @@
+
+
+const indexCtrl = {};
+
+indexCtrl.renderIndex = (req, res) => {
+  res.render('index');
+};
+
+indexCtrl.renderAbout = (req, res) => {
+  res.render('about');
+};
+
+indexCtrl.renderHome = (req, res) => {
+  res.render('home');
+};
+
+indexCtrl.renderCargaHogar = (req, res) => {
+  res.render('CargaHogar');
+};
+
+
+module.exports = indexCtrl;
